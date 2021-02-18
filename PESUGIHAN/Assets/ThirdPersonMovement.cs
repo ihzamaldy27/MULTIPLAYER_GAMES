6d9bd829b7d3c5a9d1a61c8342578b7929adfdb9
Ihzam Chaidir Aldy <ihzam.aldy27@gmail.com>
2021-02-19 00:37:57 +0700

    UPDATE ANIMASI CROUCH
    Movement basic, sudah di coba + animasi, tinggal yang ngelewatin tembok (Vaulting belom).
    
    Interect item on the way
    
    19/Februari/2021 Jam 00.37

--- PESUGIHAN/Assets/ThirdPersonMovement.cs
+++ PESUGIHAN/Assets/ThirdPersonMovement.cs
@@ -6,20 +6,23 @@ public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
-    //public Animator anim;
+    public Animator anim;
 
     public float speed = 6f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
+    public float mSpeedY = 0;
+    float mGravity = -9.81f;
+
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
 
-        /*anim = GetComponent<Animator>();
-        Debug.Log(anim);*/
+        anim = GetComponent<Animator>();
+        Debug.Log(anim);
     }
 
     // Update is called once per frame
@@ -28,29 +31,52 @@ public class ThirdPersonMovement : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        if (!controller.isGrounded)
+        {
+            mSpeedY += mGravity * Time.deltaTime;
+        }
+        else {
+            mSpeedY = 0;
+        }
 
-        if (direction.magnitude >= 0.1f) {
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 verticalMovement = Vector3.up * mSpeedY;
+
+        if (direction.magnitude >= 0.1f)
+        {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angel = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angel, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir * speed * Time.deltaTime);
+            controller.Move(verticalMovement + (moveDir * speed) * Time.deltaTime);
+            anim.SetFloat("Speed", 1f);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = 3f;
+                anim.SetFloat("Speed", 0.6f);
+            }
+            else {
+                speed = 6f;
+            }
+        }
+        
+        else {
+            anim.SetFloat("Speed", 0);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetFloat("Speed", 0.3f);
+            }
+            else
+            {
+                anim.SetFloat("Speed", 0);
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.Escape)) {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
-
-        /*if (Input.GetKey("w")) {
-            anim.SetBool("isRun", true);
-        }
-
-        if (!Input.GetKey("w"))
-        {
-        }*/
 
     }
 }
