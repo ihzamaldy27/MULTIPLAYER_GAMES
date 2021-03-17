96a26f0f1063a94217ae937d1f6b85c4ca78dab0
Ihzam Chaidir Aldy <ihzam.aldy27@gmail.com>
2021-03-17 08:23:32 +0700

    Update Cicilan
    Baru update yang berada didalam Asset(belum dalam subfoldernya)

---
+++ PESUGIHAN/AnimatiorController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatiorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Animator anim;
+    int isRunnerHash;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Debug.Log(anim);
+        isRunnerHash = Animator.StringToHash("isRun");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isRunner = anim.GetBool(isRunnerHash);
+        bool pressButton = Input.GetKey("w");
+        if (!isRunner && pressButton) {
+            anim.SetBool(isRunnerHash, true);
+        }
+
+        if (isRunner && !pressButton) {
+            anim.SetBool(isRunnerHash, false);
+        }
+    }
+}
---
+++ PESUGIHAN/ThirdPersonMovement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    public Animator anim;
+
+    public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    public float mSpeedY = 0;
+    float mGravity = -9.81f;
+    int a = 0;
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        anim = GetComponent<Animator>();
+        Debug.Log(anim);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (!isLocalPlayer)
+        {
+
+        }*/
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (!controller.isGrounded)
+        {
+            mSpeedY += mGravity * Time.deltaTime;
+        }
+        else {
+            mSpeedY = 0;
+        }
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 verticalMovement = Vector3.up * mSpeedY;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angel = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angel, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(verticalMovement + (moveDir * speed) * Time.deltaTime);
+            anim.SetFloat("Speed", 1f);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = 3f;
+                anim.SetFloat("Speed", 0.6f);
+            }
+            else {
+                speed = 6f;
+            }
+        }
+        
+        else {
+            anim.SetFloat("Speed", 0);
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                anim.SetFloat("Speed", 0.3f);
+            }
+            else
+            {
+                anim.SetFloat("Speed", 0);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            
+        }
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
