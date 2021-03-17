695d8362aec0d5c8af39ed5a93981fd66c35c615
Ihzam Chaidir Aldy <ihzam.aldy27@gmail.com>
2021-03-17 08:34:23 +0700

    Update Scripts

---
+++ PESUGIHAN/Assets/Script/ItemDestroy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroy : MonoBehaviour
+{
+    public ItemData itemData;
+    public GameObject itemObjek;
+    public Transform setPoint;
+    void Start()
+    {
+        itemObjek.GetComponent<ItemData>();
+    }
+
+    void Update()
+    {
+        if ((Input.GetKeyDown(KeyCode.E)) && (!itemData.pickUp))
+        {
+            if (itemData.pickUp)
+            {
+                itemData.destroyItem = false;
+                Debug.Log("Item gagal dihancurkan");
+                StopCoroutine(itemDestroy(itemObjek.gameObject));
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Pembakaran")
+        {
+            Debug.Log("Item ini siap dihancurkan");
+            itemData.destroyItem = true;
+            StartCoroutine(itemDestroy(itemObjek.gameObject));
+        }
+    }
+
+    IEnumerator itemDestroy(GameObject item)
+    {
+         yield return new WaitForSeconds(5);
+         Destroy(item);
+    }
+}
---
+++ PESUGIHAN/Assets/Script/Vaultable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vaultable : MonoBehaviour
+{
+    Vaultable vaulting;
+    public bool lompat = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Vault")
+        {
+            lompat = true;
+            if (lompat)
+            {
+                Debug.Log("Player siap Vaulting");
+            }
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Vault")
+        {
+            lompat = false;
+            if (!lompat)
+            {
+                Debug.Log("Player meninggalkan Vault");
+            }
+        }
+    }
+}
